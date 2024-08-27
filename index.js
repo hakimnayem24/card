cd4d4146e2c6f2037f06fe9386358b40c7ecb3ca
hakimnayem24 <nayemm12395@gmail.com>
2024-08-27 23:18:55 +0600

    first commit

--- index.js
+++ index.js
@@ -1,68 +1,195 @@
-const spoon = document.getElementById("spoon")
-const olList = document.getElementById("ol-list")
-const apply = document.getElementById("apply")
-const coupon = document.getElementById("coupon")
+const card1 = document.getElementById("card1")
+const cardName1 = document.getElementById("card-name1")
+const cardMoney1 = parseFloat(document.getElementById("card-money1").innerText).toFixed(2)
+const couponCode = document.getElementById("coupon-code")
+const applyButton = document.getElementById("apply-button")
+const addName = document.getElementById("add-name")
+const totalPrice = document.getElementById("total-price")
+const discountPrice = document.getElementById("discount-price")
+const intotalPrice = document.getElementById("intotal-price")
+const makePurchase = document.getElementById("make-purchase")
+const goHome = document.getElementById("go-home")
 
-const congratulations = document.getElementById("congratulations")
-const headingHomeCooker = document.getElementById("heading-home cooker")
-const headingSports = document.getElementById("heading sports")
-const headingCook = document.getElementById("heading cook") 
-const board = document.getElementById("board")
-const pen = document.getElementById("pen")
-const cap = document.getElementById("cap")
-const jersey = document.getElementById("jersey")
-const cates = document.getElementById("cates")
-const normalChair = document.getElementById("normal-chair")
-const chair = document.getElementById("chair")
-const sofa = document.getElementById('sofa')
-spoon.addEventListener("click", function(){
-    const li = document.createElement("li")
-    li.innerText = headingHomeCooker.innerText
-    olList.appendChild(li)
-})
-spoon.addEventListener("click", function(){
-    const li = document.createElement("li")
-    li.innerText = headingSports.innerText
-    olList.appendChild(li)
-})
-spoon.addEventListener("click", function(){
-    const li = document.createElement("li")
-    li.innerText = headingCook.innerText
-    olList.appendChild(li)
-})
-board.addEventListener("click", function(){
-    const li = document.createElement("li")
-    li.innerText = headingHomeCooker.innerText
-    olList.appendChild(li)
-})
-board.addEventListener("click", function(){
-    const li = document.createElement("li")
-    li.innerText = headingSports.innerText
-    olList.appendChild(li)
-})
-board.addEventListener("click", function(){
-    const li = document.createElement("li")
-    li.innerText = headingCook.innerText
-    olList.appendChild(li)
-})
+const card2 = document.getElementById("card2")
+const cardName2 = document.getElementById("card-name2")
+const cardMoney2 = parseFloat(document.getElementById("card-money2").innerText).toFixed(2)
+const card3 = document.getElementById("card3")
+const cardName3 = document.getElementById("card-name3")
+const cardMoney3 = parseFloat(document.getElementById("card-money3").innerText).toFixed(2)
+const card4 = document.getElementById("card4")
+const cardName4 = document.getElementById("card-name4")
+const cardMoney4 = parseFloat(document.getElementById("card-money4").innerText).toFixed(2)
+const card5 = document.getElementById("card5")
+const cardName5 = document.getElementById("card-name5")
+const cardMoney5 = parseFloat(document.getElementById("card-money5").innerText).toFixed(2)
+const card6 = document.getElementById("card6")
+const cardName6 = document.getElementById("card-name6")
+const cardMoney6 = parseFloat(document.getElementById("card-money6").innerText).toFixed(2)
+const card7 = document.getElementById("card7")
+const cardName7 = document.getElementById("card-name7")
+const cardMoney7 = parseFloat(document.getElementById("card-money7").innerText).toFixed(2)
+const card8 = document.getElementById("card8")
+const cardName8 = document.getElementById("card-name8")
+const cardMoney8 = parseFloat(document.getElementById("card-money8").innerText).toFixed(2)
+const card9 = document.getElementById("card9")
+const cardName9 = document.getElementById("card-name9")
+const cardMoney9 = parseFloat(document.getElementById("card-money9").innerText).toFixed(2)
+let i = 0 ;
 
-apply.addEventListener("click", function(){
-    if(coupon.value == "SELL200"){
-        apply.setAttribute("disabled" , true)
-        apply.classList.add("text-red-500")
-        apply.classList.remove("btn-secondary")
+
+// card1.addEventListener("click", function(){
+//     addName.innerText = cardName1.innerText ;
+//     i++ ;
+
+//     const p = document.createElement("p")
+//     p.innerText = i +"." + cardName1.innerText ;
+//     addName.appendChild(p);
+// })
+
+card1.addEventListener("click", function(){
+    addName.innerText = cardName1.innerText ;
+    const floatTotalPrice = parseFloat(totalPrice.innerText).toFixed(2)
+    totalPrice.innerText = parseFloat(cardMoney1) + parseFloat(floatTotalPrice) ;
+    makePurchase.removeAttribute("disabled")
+    if(parseFloat(totalPrice.innerText) > 200){
+        applyButton.removeAttribute("disabled")
     }
-    else{
-        coupon.value = ""
-    }
+    i++ ;
+    const p = document.createElement("p")
+    p.innerText = i +"." + cardName1.innerText ;
+    addName.appendChild(p);
 })
-const price = document.getElementById("price")
-const discount = document.getElementById("discount")
-const total = document.getElementById("total")
-let priceValue = parseFloat(price.innerText)
-let discountValue = parseFloat(discount.innerText)
-let totalValue = parseFloat(total.innertext)
-const mainClick = document.getElementById("mainclick")
+
+card2.addEventListener("click", function(){
+    addName.innerText = cardName2.innerText ;
+    const floatTotalPrice = parseFloat(totalPrice.innerText).toFixed(2)
+    totalPrice.innerText = parseFloat(cardMoney2) + parseFloat(floatTotalPrice) ;
+    makePurchase.removeAttribute("disabled")
+    if(parseFloat(totalPrice.innerText) > 200){
+        applyButton.removeAttribute("disabled")
+    }
+    i++ ;
+    const p = document.createElement("p")
+    p.innerText = i +"." + cardName2.innerText ;
+    addName.appendChild(p);
+})
+card3.addEventListener("click", function(){
+    addName.innerText = cardName3.innerText ;
+    const floatTotalPrice = parseFloat(totalPrice.innerText).toFixed(2)
+    totalPrice.innerText = parseFloat(cardMoney3) + parseFloat(floatTotalPrice) ;
+    makePurchase.removeAttribute("disabled")
+    if(parseFloat(totalPrice.innerText) > 200){
+        applyButton.removeAttribute("disabled")
+    }
+    i++ ;
+    const p = document.createElement("p")
+    p.innerText = i +"." + cardName3.innerText ;
+    addName.appendChild(p);
+})
+card4.addEventListener("click", function(){
+    addName.innerText = cardName4.innerText ;
+    const floatTotalPrice = parseFloat(totalPrice.innerText).toFixed(2)
+    totalPrice.innerText = parseFloat(cardMoney4) + parseFloat(floatTotalPrice) ;
+    makePurchase.removeAttribute("disabled")
+    if(parseFloat(totalPrice.innerText) > 200){
+        applyButton.removeAttribute("disabled")
+    }
+    i++ ;
+    const p = document.createElement("p")
+    p.innerText = i +"." + cardName4.innerText ;
+    addName.appendChild(p);
+})
+card5.addEventListener("click", function(){
+    addName.innerText = cardName5.innerText ;
+    const floatTotalPrice = parseFloat(totalPrice.innerText).toFixed(2)
+    totalPrice.innerText = parseFloat(cardMoney5) + parseFloat(floatTotalPrice) ;
+    makePurchase.removeAttribute("disabled")
+    if(parseFloat(totalPrice.innerText) > 200){
+        applyButton.removeAttribute("disabled")
+    }
+    i++ ;
+    const p = document.createElement("p")
+    p.innerText = i +"." + cardName5.innerText ;
+    addName.appendChild(p);
+})
+card6.addEventListener("click", function(){
+    addName.innerText = cardName6.innerText ;
+    const floatTotalPrice = parseFloat(totalPrice.innerText).toFixed(2)
+    totalPrice.innerText = parseFloat(cardMoney6) + parseFloat(floatTotalPrice) ;
+    m
+    makePurchase.removeAttribute("disabled")
+    if(parseFloat(totalPrice.innerText) > 200){
+        applyButton.removeAttribute("disabled")
+    }
+    i++ ;
+    const p = document.createElement("p")
+    p.innerText = i +"." + cardName6.innerText ;
+    addName.appendChild(p);
+})
+card7.addEventListener("click", function(){
+    addName.innerText = cardName7.innerText ;
+    const floatTotalPrice = parseFloat(totalPrice.innerText).toFixed(2)
+    totalPrice.innerText = parseFloat(cardMoney7) + parseFloat(floatTotalPrice) ;
+    makePurchase.removeAttribute("disabled")
+    if(parseFloat(totalPrice.innerText) > 200){
+        applyButton.removeAttribute("disabled")
+    }
+    i++ ;
+    const p = document.createElement("p")
+    p.innerText = i +"." + cardName7.innerText ;
+    addName.appendChild(p);
+})
+card8.addEventListener("click", function(){
+    addName.innerText = cardName8.innerText ;
+    const floatTotalPrice = parseFloat(totalPrice.innerText).toFixed(2)
+    totalPrice.innerText = parseFloat(cardMoney8) + parseFloat(floatTotalPrice) ;
+    makePurchase.removeAttribute("disabled")
+    if(parseFloat(totalPrice.innerText) > 200){
+        applyButton.removeAttribute("disabled")
+    }
+    i++ ;
+    const p = document.createElement("p")
+    p.innerText = i +"." + cardName8.innerText ;
+    addName.appendChild(p);
+})
+card9.addEventListener("click", function(){
+    addName.innerText = cardName9.innerText ;
+    const floatTotalPrice = parseFloat(totalPrice.innerText).toFixed(2)
+    totalPrice.innerText = parseFloat(cardMoney9) + parseFloat(floatTotalPrice) ;
+    makePurchase.removeAttribute("disabled")
+    if(parseFloat(totalPrice.innerText) > 200){
+        applyButton.removeAttribute("disabled")
+    }
+    i++ ;
+    const p = document.createElement("p")
+    p.innerText = i +"." + cardName9.innerText ;
+    addName.appendChild(p);
+})
 
 
+
+applyButton.addEventListener("click", function(){
+    if(couponCode.value == "SELL200"){
+        const discount =  (parseInt(totalPrice.innerText) * 20) / 100 ;
+        discountPrice.innerText = discount ;
+        
+       
+        const InTotal = totalPrice.innerText - discountPrice.innerText
+            intotalPrice.innerText = InTotal.toFixed(2);
     
+    
+        applyButton.setAttribute("disabled", true)
+    
+    }
+    couponCode.value = "" ;
+})
+
+goHome.addEventListener("click" , function(){
+    applyButton.setAttribute("disabled", true)
+    addName.innerText = ""
+    totalPrice.innerText = "00.00"
+    discountPrice.innerText = "00.00"
+    intotalPrice.innerText = "00.00"
+    makePurchase.setAttribute("disabled", true)
+})
+
